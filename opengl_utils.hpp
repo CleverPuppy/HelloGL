7c993031ee630285fac6605ec4e72944286b3ed8
cleverpuppy <919479850@qq.com>
2023-02-22 14:33:55 +0800

    feature: add linux x11 egl support

--- opengl_utils.hpp
+++ opengl_utils.hpp
@@ -7,12 +7,22 @@
 #include <GLFW/glfw3.h>
 #endif
 
+#ifdef __linux__
+#include <X11/Xlib.h>
+#include <EGL/egl.h>
+#include "unistd.h"
+#endif
+
 #include <array>
+#include <vector>
 #include <cstdio>
 #include <functional>
 #include <iostream>
 #include <string>
 #include <sstream>
+#include <cassert>
+
+#define Assert(x) assert(x)
 
 #define __valid_or_ret(val)                                                    \
   if (!valid)                                                                  \
@@ -176,14 +186,22 @@ private:
   inline void MakeExistShader(int idx) { shader_exists_status[idx] = true; }
 };
 
+class XHelper
+{
+  public:
+    virtual bool InitWindow(int width, int height, const std::string& windowTitle,
+        const GLProgramVersion& glVersion) = 0;
+    virtual void Render(std::function<void()>&& xFunc) = 0;
+};
+
 #ifdef USE_GLFW
-class GLFWHelper {
+class GLFWHelper : public XHelper{
 public:
   GLFWHelper() = default;
   ~GLFWHelper() = default;
 
-  inline bool InitWindow(int width, int height, const std::string& windowTitle,
-      const GLProgramVersion& glVersion)
+  virtual bool InitWindow(int width, int height, const std::string& windowTitle,
+      const GLProgramVersion& glVersion) override
   {
       return InitWindow(width, height, windowTitle,
           glVersion.eType,
@@ -243,7 +261,7 @@ public:
     return true;
   }
 
-  inline void Render(std::function<void()> &&funcRender) {
+  virtual void Render(std::function<void()> &&funcRender) override {
     while (!glfwWindowShouldClose(window)) {
       // handle input
       GLFWHelper::process_input(window);
@@ -273,4 +291,312 @@ public:
   int height;
   GLFWwindow *window;
 };
+#endif
+
+#ifdef __linux__
+
+class EglAttrBuilder
+{
+public:
+    EglAttrBuilder(): bFinalised(false) {}
+    ~EglAttrBuilder() = default;
+
+    inline void AppendAttr(EGLint param, EGLint value)
+    {
+        if(bFinalised){
+            Assert(0 && "AppendAttr in finalized attr.");
+            return;
+        }
+
+        data.push_back(param);
+        data.push_back(value);
+    }
+
+    inline EGLint* GetAttr()
+    {
+        if(!bFinalised)
+        {
+            makeFinalize();
+        }
+        return data.data();
+    }
+
+private:
+    bool bFinalised;
+    std::vector<EGLint> data;
+
+    inline void makeFinalize()
+    {
+        if(bFinalised) return;
+        data.push_back(EGL_NONE);
+    }
+};
+
+struct X11Window
+{
+    Display* dpy;
+    Window wnd;
+    Atom WM_PROTOCOLS;
+    Atom WM_DELETE_WINDOW;
+};
+
+struct EglInfo
+{
+    EGLConfig config;
+    EGLDisplay *eglDisplay;
+    EGLSurface *eglSurface;
+    EGLContext *eglContext;
+    int vsync;
+};
+
+static X11Window InitX11(uint width, uint height, const char* strWindowName)
+{
+    Display* dpy = XOpenDisplay(NULL);
+    Assert(dpy && "Cannot open X display");
+
+    XSetWindowAttributes attributes =
+    {
+        .event_mask = StructureNotifyMask,
+    };
+
+    // create window
+
+    Window window = XCreateWindow(
+        dpy, DefaultRootWindow(dpy),
+        0, 0, width, height,
+        0, CopyFromParent, InputOutput, CopyFromParent, CWEventMask,
+        &attributes);
+    Assert(window && "Failed to create window");
+
+    // uncomment in case you want fixed size window
+    //XSizeHints* hints = XAllocSizeHints();
+    //Assert(hints);
+    //hints->flags |= PMinSize | PMaxSize;
+    //hints->min_width  = hints->max_width  = width;
+    //hints->min_height = hints->max_height = height;
+    //XSetWMNormalHints(dpy, window, hints);
+    //XFree(hints);
+
+    // set window title
+    XStoreName(dpy, window, "OpenGL Window");
+
+    // subscribe to window close notification
+    Atom WM_PROTOCOLS = XInternAtom(dpy, "WM_PROTOCOLS", False);
+    Atom WM_DELETE_WINDOW = XInternAtom(dpy , "WM_DELETE_WINDOW", False);
+    XSetWMProtocols(dpy, window, &WM_DELETE_WINDOW, 1);
+
+    X11Window ret;
+    ret.wnd = window;
+    ret.dpy = dpy;
+    ret.WM_DELETE_WINDOW = WM_DELETE_WINDOW;
+    ret.WM_PROTOCOLS = WM_PROTOCOLS;
+    return ret;
+}
+
+static EglInfo InitEgl(const X11Window& x11Window, API_TYPE type, int major, int minor)
+{
+    std::cout << "Creating EGL " << (type == API_TYPE::OGL ? "GL" : " GLES") << std::endl;
+
+    Display *dpy = x11Window.dpy;
+    // initialize EGL
+    EGLDisplay* display;
+    {
+        display = (EGLDisplay*)eglGetDisplay((EGLNativeDisplayType)dpy);
+        Assert(display != EGL_NO_DISPLAY && "Failed to get EGL display");
+
+        EGLint major, minor;
+        if (!eglInitialize(display, &major, &minor))
+        {
+            Assert(0 && "Cannot initialize EGL display");
+        }
+        if (major < 1 || (major == 1 && minor < 5))
+        {
+            Assert(0 && "EGL version 1.5 or higher required");
+        }
+    }
+
+    // Bind API
+    EGLBoolean ok = eglBindAPI( (type == API_TYPE::OGL ?  EGL_OPENGL_API : EGL_OPENGL_ES_API));
+    Assert(ok && "Failed to eglBindAPI for EGL");
+
+    // choose EGL configuration
+    EGLConfig config;
+    {
+        EglAttrBuilder attrBuilder;
+
+        attrBuilder.AppendAttr( EGL_SURFACE_TYPE, EGL_WINDOW_BIT );
+        if(type == API_TYPE::OGL)
+        {
+            attrBuilder.AppendAttr(EGL_CONFORMANT, EGL_OPENGL_BIT);
+            attrBuilder.AppendAttr(EGL_RENDERABLE_TYPE, EGL_OPENGL_BIT);
+        }
+        else
+        {
+            attrBuilder.AppendAttr(EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT);
+        }
+        attrBuilder.AppendAttr(EGL_COLOR_BUFFER_TYPE, EGL_RGB_BUFFER);
+        attrBuilder.AppendAttr(EGL_RED_SIZE, 8);
+        attrBuilder.AppendAttr(EGL_GREEN_SIZE, 8);
+        attrBuilder.AppendAttr(EGL_BLUE_SIZE, 8);
+        attrBuilder.AppendAttr(EGL_DEPTH_SIZE, 24);
+        attrBuilder.AppendAttr(EGL_STENCIL_SIZE, 8);
+        //     // uncomment for multisampled framebuffer
+        //     //EGL_SAMPLE_BUFFERS, 1,
+        //     //EGL_SAMPLES,        4, // 4x MSAA
+
+        EGLint count;
+        if (!eglChooseConfig(display, attrBuilder.GetAttr(), &config, 1, &count) || count != 1)
+        {
+            Assert(0 && "Cannot choose EGL config");
+        }
+    }
+
+    // create EGL surface
+    EGLSurface* surface;
+    {
+        EglAttrBuilder attrBuilder;
+        attrBuilder.AppendAttr(EGL_GL_COLORSPACE, EGL_GL_COLORSPACE_LINEAR);
+        attrBuilder.AppendAttr(EGL_RENDER_BUFFER, EGL_BACK_BUFFER);
+
+        // EGLint attr[] =
+        // {
+        //     EGL_GL_COLORSPACE, EGL_GL_COLORSPACE_LINEAR, // or use EGL_GL_COLORSPACE_SRGB for sRGB framebuffer
+        //     EGL_RENDER_BUFFER, EGL_BACK_BUFFER,
+        //     EGL_NONE,
+        // };
+
+        surface = (EGLSurface*)eglCreateWindowSurface(display, config, x11Window.wnd, attrBuilder.GetAttr());
+        if (surface == EGL_NO_SURFACE)
+        {
+            Assert(0 && "Cannot create EGL surface");
+        }
+    }
+
+    // create EGL context
+    EGLContext* context;
+    {
+        EglAttrBuilder attrBuilder;
+        attrBuilder.AppendAttr(EGL_CONTEXT_MAJOR_VERSION, major);
+        attrBuilder.AppendAttr(EGL_CONTEXT_MINOR_VERSION, minor);
+        if(type == API_TYPE::OGL)
+        {
+            attrBuilder.AppendAttr(EGL_CONTEXT_OPENGL_PROFILE_MASK, EGL_CONTEXT_OPENGL_CORE_PROFILE_BIT);
+        }
+#ifndef NDEBUG
+        attrBuilder.AppendAttr(EGL_CONTEXT_OPENGL_DEBUG, EGL_TRUE);
+#endif
+
+        context = (EGLContext*)eglCreateContext(display, config, EGL_NO_CONTEXT, attrBuilder.GetAttr());
+        Assert(context != EGL_NO_CONTEXT && "Cannot create EGL context, Version not supported?");
+    }
+
+    ok = eglMakeCurrent(display, surface, surface, context);
+    Assert(ok && "Failed to make context current");
+
+    // use 0 to disable vsync
+    int vsync = 1;
+    ok = eglSwapInterval(display, vsync);
+    Assert(ok && "Failed to set vsync for EGL");
+
+    EglInfo ret;
+    ret.config = config;
+    ret.eglSurface = surface;
+    ret.eglDisplay = display;
+    ret.eglContext = context;
+    ret.vsync = vsync;
+
+    return ret;
+}
+
+class X11EglHelper: public XHelper
+{
+public:
+  X11EglHelper() = default;
+
+  virtual bool InitWindow(int width, int height, const std::string& windowTitle,
+      const GLProgramVersion& glVersion) override
+  {
+    x11Window = InitX11(width, height, windowTitle.c_str());
+    eglInfo = InitEgl(x11Window, glVersion.eType, glVersion.major, glVersion.minor);
+#ifdef USE_GLAD
+    if (!gladLoadGLLoader((GLADloadproc)eglGetProcAddress)) {
+      std::cerr << "gladLoadGLLoader failed." << std::endl;
+      eglTerminate(eglInfo.eglDisplay);
+      return false;
+    }
+#endif
+    return true;
+  }
+
+  virtual void Render(std::function<void()>&& xFunc) override
+  {
+    DoCommonRender(std::forward<std::function<void()>&&>(xFunc));
+  }
+
+  X11Window x11Window;
+  EglInfo eglInfo;
+private:
+  void DoCommonRender(std::function<void()>&& xRenderFunc)
+  {
+      Display *dpy = x11Window.dpy;
+      Window window = x11Window.wnd;
+      EGLSurface *surface = eglInfo.eglSurface;
+      EGLDisplay *display = eglInfo.eglDisplay;
+      int vsync = eglInfo.vsync;
+
+      // show the window
+      XMapWindow(dpy, window);
+      for (;;)
+      {
+          // process all incoming X11 events
+          if (XPending(dpy))
+          {
+              XEvent event;
+              XNextEvent(dpy, &event);
+              if (event.type == ClientMessage)
+              {
+                  if (event.xclient.message_type == x11Window.WM_PROTOCOLS)
+                  {
+                      Atom protocol = event.xclient.data.l[0];
+                      if (protocol == x11Window.WM_DELETE_WINDOW)
+                      {
+                          // window closed, exit the for loop
+                          break;
+                      }
+                  }
+              }
+              continue;
+          }
+
+          // get current window size
+          XWindowAttributes attr;
+          Status status = XGetWindowAttributes(dpy, window, &attr);
+          Assert(status && "Failed to get window attributes");
+
+          int width = attr.width;
+          int height = attr.height;
+
+          // render only if window size is non-zero
+          if (width != 0 && height != 0)
+          {
+              xRenderFunc();
+
+              // swap the buffers to show output
+              if (!eglSwapBuffers(display, surface))
+              {
+                  Assert(0 && "Failed to swap OpenGL buffers!");
+              }
+          }
+          else
+          {
+              // window is minimized, cannot vsync - instead sleep a bit
+              if (vsync)
+              {
+                  usleep(10 * 1000);
+              }
+          }
+      }
+  }
+};
+
 #endif
---
+++ triangle_x11egl.cpp
@@ -0,0 +1,64 @@
+#include "opengl_utils.hpp"
+#include <cassert>
+#include <cstddef>
+
+int main()
+{
+    GLProgramVersion glVersion{ API_TYPE::OGL, 3, 3 };
+    X11EglHelper x11EglHelper;
+    x11EglHelper.InitWindow(500, 400, "test", glVersion);
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    struct Triangle {
+        vec2 pos;
+        vec3 color;
+    } triangles[3] = {
+        {-0.7f, -0.2, 1.0f, 0.0f, 0.0f},
+        {0.7f, -0.2, 0.0f, 1.0f, 0.0f},
+        {0.0f, 0.5, 0.0f, 0.0f, 1.0f},
+    };
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Triangle), (void*)0);
+    glEnableVertexAttribArray(0);
+
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, sizeof(Triangle), (void*)sizeof(vec2));
+    glEnableVertexAttribArray(1);
+
+    GLProgramGenerator programGen;
+    GLuint program = programGen.AppendShader(glVersion, GL_VERTEX_SHADER, R"(
+            layout(location = 0) in vec2 Pos;
+            layout(location = 1) in vec3 Color;
+            out vec4 out_Color;
+            void main()
+            {
+                gl_Position = vec4(Pos, 1, 1);
+                out_Color = vec4(Color, 1);
+            }
+        )").AppendShader(glVersion, GL_FRAGMENT_SHADER, R"(
+            in vec4 out_Color;
+            out vec4 color;
+            void main()
+            {
+                color = out_Color;
+            }
+        )").AttachAndLink();
+    assert(program != (GLuint)-1);
+
+    x11EglHelper.Render([&glVersion, program](){
+        glClearColor(0.2, 0.3, 0.4, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(program);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    });
+
+    return 0;
+}
