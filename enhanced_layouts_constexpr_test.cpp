dab1dac4cc59f7b207a35aa9dccb551a3e44b1da
cleverpuppy <919479850@qq.com>
2024-04-30 15:36:57 +0800

    add enhanced layouts constexpr test

---
+++ enhanced_layouts_constexpr_test.cpp
@@ -0,0 +1,67 @@
+#include "opengl_utils.hpp"
+#include <cassert>
+#include <cstddef>
+
+int main()
+{
+    GLProgramVersion glVersion{ API_TYPE::OGL, 4, 4 };
+    // GLProgramVersion glVersion{ API_TYPE::GLES, 3, 2 };
+    GLFWHelper glfwHelper;
+    glfwHelper.InitWindow(500, 400, "test", glVersion);
+
+    GLuint VAO, VBO;
+
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    struct Triangle {
+        vec2 pos;
+        vec3 color;
+    } triangles[3] = {
+        {-0.7f, -0.2, 1.0f, 0.0f, 0.0f},
+        {0.7f, -0.2, 0.0f, 1.0f, 0.0f},
+        {0.0f, 0.5, 0.0f, 0.0f, 1.0f},
+    };
+
+    glGenBuffers(1, &VBO);
+    glBindBuffer(GL_ARRAY_BUFFER, VBO);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(triangles), triangles, GL_STATIC_DRAW);
+
+    const int locationBase = 2;
+    glVertexAttribPointer(locationBase + 0, 2, GL_FLOAT, GL_FALSE, sizeof(Triangle), (void*)0);
+    glEnableVertexAttribArray(locationBase + 0);
+
+    glVertexAttribPointer(locationBase + 1, 3, GL_FLOAT, GL_FALSE, sizeof(Triangle), (void*)sizeof(vec2));
+    glEnableVertexAttribArray(locationBase + 1);
+
+    GLProgramGenerator programGen;
+    GLuint program = programGen.AppendShader(glVersion, GL_VERTEX_SHADER, R"(
+            const int locationBase = 2;
+            layout(location = locationBase + 0) in vec2 Pos;
+            layout(location = locationBase + 1) in vec3 Color;
+            out vec4 out_Color;
+            void main()
+            {
+                gl_Position = vec4(Pos, 1, 1);
+                out_Color = vec4(Color, 1);
+            }
+        )").AppendShader(glVersion, GL_FRAGMENT_SHADER, R"(
+            precision highp float;
+            in vec4 out_Color;
+            out vec4 color;
+            void main()
+            {
+                color = out_Color;
+            }
+        )").AttachAndLink();
+    assert(program != (GLuint)-1);
+
+    glfwHelper.Render([&glVersion, program](){
+        glClearColor(0.2, 0.3, 0.4, 1.0);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(program);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+    });
+
+    return 0;
+}
